ef6446cb2f5b58e3175799be0cdb9460bccffabb
salahmak <crsalah3@gmail.com>
2020-07-29 01:32:01 +0100

    trying to fix origin problem

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const DB_URL = process.env.DB_URL;
 const app = express();
 app.use(
     cors({
-        origin: "https://refinity-cpanel-q2suzfbui.vercel.app",
+        origin: "*",
         credentials: true,
     })
 );
