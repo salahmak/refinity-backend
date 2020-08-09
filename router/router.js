07a6197398b602d7eb701c9f99df3fdbf4a2ffd2
salahmak <crsalah3@gmail.com>
2020-08-09 16:00:57 +0100

    added rateLimiter to the /forms route to prevent spam attacks

--- router/router.js
+++ router/router.js
@@ -3,8 +3,16 @@ const auth = require("./routes/auth.js");
 const forms = require("./routes/forms.js");
 const panel = require("./routes/panel.js");
 
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+	windowMs: 60000, //1 min
+	max: 2, // only allow 2 requests per minute from the same ip address
+	message: { msg: "too many requests" },
+});
+
 router.use("/auth/", auth);
-router.use("/forms/", forms);
+router.use("/forms/", limiter, forms);
 router.use("/panel/", panel);
 
 module.exports = router;
