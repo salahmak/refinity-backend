7c67e93dca0b047ab1e1c66017540f4265656966
salahmak <crsalah3@gmail.com>
2020-08-24 17:31:34 +0100

    switched to oauth2 in nodemailer

--- nodemailer/enrollMail.js
+++ nodemailer/enrollMail.js
@@ -1,6 +1,16 @@
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
 module.exports = async (enroll) => {
+    const myOAuth2Client = new OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
+
+    myOAuth2Client.setCredentials({
+        refresh_token: process.env.REFRESH_TOKEN,
+    });
+
+    const myAccessToken = myOAuth2Client.getAccessToken();
+
     const {
         id,
         type,
@@ -24,8 +34,12 @@ module.exports = async (enroll) => {
     const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASS,
+            type: "OAuth2",
+            user: process.env.EMAIL, //your gmail account you used to set the project up in google cloud console"
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            refreshToken: process.env.REFRESH_TOKEN,
+            accessToken: myAccessToken, //access token variable we defined earlier
         },
     });
 
